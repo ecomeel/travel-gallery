0650efe991f5befb22926978c053fa0fc0fac517
ecomeel <ecomeel@gmail.com>
2023-08-29 18:49:34 +0300

    gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,45 +1,58 @@
 import React from "react";
 import TravelCard from "../TravelCard/TravelCard";
-import './Gallery.css'
+import "./Gallery.css";
 
 const trips = [
     {
-        city: 'Тбилиси',
+        key: 1,
+        city: "Тбилиси",
         month: "апрель",
         price: "83.000",
         transportType: "на автобусе",
-        peopleAmount: 10 
+        peopleAmount: 10,
     },
     {
+        key: 2,
         city: "Стамбул",
         month: "март",
         price: "110.000",
         transportType: "на самолете",
-        // peopleAmount: 
+        peopleAmount: undefined
     },
     {
+        key: 3,
         city: "Дубай",
         month: "июнь",
         price: "220.000",
         transportType: "на самолете",
-        peopleAmount: 15
+        peopleAmount: 15,
     },
     {
+        key: 4,
         city: "Пхукет",
         month: "сентябрь",
         price: "135.000",
         transportType: "самолет + паром",
-        peopleAmount: 11
+        peopleAmount: 11,
     },
-]
+];
 
 export default function Gallery() {
+    const tripsHTML = trips.map( trip => (
+        <TravelCard
+            key = {trip.key}
+            city = {trip.city}
+            month = {trip.month}
+            price = {trip.price}
+            transportType = {trip.transportType}
+            peopleAmount = {trip.peopleAmount}
+        />
+
+    ))
+    
     return (
         <ul className="gallery">
-            <TravelCard />
-            <TravelCard />
-            <TravelCard />
-            <TravelCard />
+            {tripsHTML}
         </ul>
     );
 }
--- src/components/TravelCard/TravelCard.jsx
+++ src/components/TravelCard/TravelCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import img1 from "../../assets/pict1.png";
+import './TravelCard.css'
 
-export default function TravelCard() {
-    return <img src={img1} alt="" />
+export default function TravelCard(props) {
+    return (
+        <li className="travelCard">
+            <h1>{props.city}</h1>
+        </li>
+    )
 }
 
