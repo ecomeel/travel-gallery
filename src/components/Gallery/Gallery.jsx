cef841ec0e12079fef0a36580e8e936ec7a5767a
ecomeel <ecomeel@gmail.com>
2023-08-29 19:13:48 +0300

    added info

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import TravelCard from "../TravelCard/TravelCard";
+import TbilissiImg from "../../assets/pict1.png";
+import StamboulImg from "../../assets/pict2.png";
+import DubaiImg from "../../assets/pict3.png";
+import Phuket from "../../assets/pict4.png";
 import "./Gallery.css";
 
 const trips = [
@@ -10,6 +14,7 @@ const trips = [
         price: "83.000",
         transportType: "на автобусе",
         peopleAmount: 10,
+        background: TbilissiImg
     },
     {
         key: 2,
@@ -17,7 +22,8 @@ const trips = [
         month: "март",
         price: "110.000",
         transportType: "на самолете",
-        peopleAmount: undefined
+        peopleAmount: undefined,
+        background: StamboulImg
     },
     {
         key: 3,
@@ -26,6 +32,7 @@ const trips = [
         price: "220.000",
         transportType: "на самолете",
         peopleAmount: 15,
+        background: DubaiImg
     },
     {
         key: 4,
@@ -34,6 +41,7 @@ const trips = [
         price: "135.000",
         transportType: "самолет + паром",
         peopleAmount: 11,
+        background: Phuket
     },
 ];
 
@@ -46,10 +54,11 @@ export default function Gallery() {
             price = {trip.price}
             transportType = {trip.transportType}
             peopleAmount = {trip.peopleAmount}
+            background = {trip.background}
         />
 
     ))
-    
+
     return (
         <ul className="gallery">
             {tripsHTML}
--- src/components/TravelCard/TravelCard.jsx
+++ src/components/TravelCard/TravelCard.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import img1 from "../../assets/pict1.png";
-import './TravelCard.css'
+import "./TravelCard.css";
 
 export default function TravelCard(props) {
     return (
-        <li className="travelCard">
-            <h1>{props.city}</h1>
+        <li
+            className="travelCard"
+            style={{
+                backgroundImage: `url(${props.background})`,
+            }}
+        >
+            <h2>{props.city}, {props.month} -- {props.price}</h2>
         </li>
-    )
+    );
 }
-
